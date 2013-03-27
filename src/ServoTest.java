bf874deec1105bcb5f56e9ff6f8f863fc555e32d
Samuel Schneider <crazysaem@gmail.com>
2013-03-27 21:04:30 +0100

    added another servo source

--- src/ServoTest.java
+++ src/ServoTest.java
@@ -15,8 +15,10 @@ public class ServoTest
                 "My LED",           // PIN FRIENDLY NAME (optional)
                 PinState.LOW);		
 		
+		//Servo sources:
 		//http://en.wikipedia.org/wiki/Servo_control
 		//http://upload.wikimedia.org/wikipedia/commons/b/b7/Sinais_controle_servomotor.JPG
+		//http://learn.adafruit.com/adafruits-raspberry-pi-lesson-8-using-a-servo-motor/servo-motors
 		//1000 == -90°
 		//1500 == 0°
 		//2000 == 90°
